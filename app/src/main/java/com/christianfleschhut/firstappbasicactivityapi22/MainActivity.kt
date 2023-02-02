4808db545a76214c29af8de962d4d1b32c2c57dc
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-02-02 20:05:30 +0100

    UI Ch02: Display Snackbar messages

--- app/src/main/java/com/christianfleschhut/firstappbasicactivityapi22/MainActivity.kt
+++ app/src/main/java/com/christianfleschhut/firstappbasicactivityapi22/MainActivity.kt
@@ -28,9 +28,10 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.fab.setOnClickListener {
+            Snackbar.make(it, "Snackbar!", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Show info") {}
+                .show()
         }
     }
 
