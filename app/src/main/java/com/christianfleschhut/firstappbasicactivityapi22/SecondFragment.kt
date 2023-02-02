0b2c21e9536f825c49bbfe60b6e155502a741898
Christian Fleschhut <weltrekordhalter@gmail.com>
2023-02-02 20:51:55 +0100

    UI Ch03: Display image resources

--- app/src/main/java/com/christianfleschhut/firstappbasicactivityapi22/SecondFragment.kt
+++ app/src/main/java/com/christianfleschhut/firstappbasicactivityapi22/SecondFragment.kt
@@ -35,6 +35,8 @@ class SecondFragment : Fragment() {
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        binding.textviewSecond.text = getString(R.string.hello_second_fragment, "abc")
     }
 
     override fun onDestroyView() {
